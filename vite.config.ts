731e08dc296af7218d16ef6b970947ce3ac86e7c
sdaubarsanjuan-commits <sdaubarsanjuan@gulliverprep.org>
2026-06-01 11:59:30 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,8 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, '.'),
       },
     },
+    build: {
+      outDir: 'dist', // Standard output directory for Vercel
+    }
   };
 });
